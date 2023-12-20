a37959fadf9f8fdf8930839da57ce5be052504d3
srinath <www.srinath.25.com@gmail.com>
2023-12-20 16:52:08 +0530

    get items from an array

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import reactImg from "./assets/react-core-concepts.png";
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+import componentImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data.js";
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
@@ -18,12 +20,47 @@ function Header() {
     </header>
   );
 }
+
+function CoreConcepts(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header></Header>
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <ul>
+            <CoreConcepts
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcepts
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}
+            />
+            <CoreConcepts
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              image={CORE_CONCEPTS[2].image}
+            />
+            <CoreConcepts
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image}
+            />
+          </ul>
+        </section>
+        <h2>Core Concepts</h2>
       </main>
     </div>
   );
